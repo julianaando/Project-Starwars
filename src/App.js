3baf3dcd732bc3cb6cbc10c2e7850bee064e881a
Juliana Ando <juandocezare@gmail.com>
2023-03-29 19:20:50 -0300

    Req 1 done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import React from 'react';
-import './App.css';
-// começando o projeto com commit inicial
+import { Switch, Route } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import PlanetContext from './context/PlanetContext';
+import Table from './components/Table';
 
 function App() {
+  const [planets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    fetch('https://swapi.dev/api/planets')
+      .then((response) => response.json())
+      .then((data) => {
+        const filteredPlanets = data.results
+          .map((planet) => {
+            const newPlanet = { ...planet };
+            delete newPlanet.residents;
+            return newPlanet;
+          });
+        setPlanets(filteredPlanets);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  const context = {
+    planets,
+  };
+
   return (
-    <span>Hello, App!</span>
+    <PlanetContext.Provider value={ context }>
+      <Switch>
+        <Route exact path="/" component={ Table } />
+      </Switch>
+    </PlanetContext.Provider>
   );
 }
 
